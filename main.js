3c87b4f366fcc7cf6c95c6b46b54125f3e10e801
Scooter <talkufy@outlook.com>
2024-06-18 18:15:57 -0400

    Added more to the main.js. Added the text on the wall, fan lights, lighting, and the start of the about me menu

--- main.js
+++ main.js
@@ -84,3 +84,491 @@ let defaultCamerRot = {
   z: 0.7240714481613063,
 };
 camera.position.set(defaultCameraPos.x, defaultCameraPos.y, defaultCameraPos.z);
+
+// RENDERER
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+// STATS
+// const stats = new Stats();
+// document.querySelector('.experience').appendChild(stats.dom);
+
+// CONTROLS
+const controls = new OrbitControls(camera, renderer.domElement);
+// controls.enableDamping = true;
+controls.enablePan = false;
+controls.minDistance = 0.9;
+controls.maxDistance = 1.6;
+controls.minAzimuthAngle = 0.2;
+controls.maxAzimuthAngle = Math.PI * 0.78;
+controls.minPolarAngle = 0.3;
+controls.maxPolarAngle = Math.PI / 2;
+controls.update();
+
+// LOAD MODEL & ASSET
+// const loadingManager = new THREE.LoadingManager();
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('draco/');
+const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoLoader);
+gltfLoader.load(
+  'models/room.glb',
+  function (room) {
+    // hide loader on loade
+    loaderWrapper.style.display = 'none';
+
+    // load video
+    const video = document.createElement('video');
+    video.src = 'textures/arcane.mp4';
+    video.muted = true;
+    video.playsInline = true;
+    video.autoplay = true;
+    video.loop = true;
+
+    // create video texture
+    const videoTexture = new THREE.VideoTexture(video);
+    videoTexture.minFilter = THREE.NearestFilter;
+    videoTexture.magFilter = THREE.NearestFilter;
+    videoTexture.generateMipmaps = false;
+    videoTexture.encoding = THREE.sRGBEncoding;
+
+    room.scene.children.forEach((child) => {
+      // disable shadow by wall
+      if (child.name !== 'Wall') {
+        child.castShadow = true;
+      }
+      child.receiveShadow = true;
+
+      if (child.children) {
+        child.children.forEach((innerChild) => {
+          // disable shadow by book cover & switch btn
+          if (innerChild.name !== 'Book001' && innerChild.name !== 'Switch') {
+            innerChild.castShadow = true;
+          }
+          innerChild.receiveShadow = true;
+        });
+      }
+
+      if (child.name === 'Stand') {
+        child.children[0].material = new THREE.MeshBasicMaterial({
+          map: videoTexture,
+        });
+        video.play();
+      }
+
+      // transparent texture for glass
+      if (child.name === 'CPU') {
+        child.children[0].material = new THREE.MeshPhysicalMaterial();
+        child.children[0].material.roughness = 0;
+        child.children[0].material.color.set(0x999999);
+        child.children[0].material.ior = 3;
+        child.children[0].material.transmission = 2;
+        child.children[0].material.opacity = 0.8;
+        child.children[0].material.depthWrite = false;
+        child.children[0].material.depthTest = false;
+        child.children[1].material = new THREE.MeshPhysicalMaterial();
+        child.children[1].material.roughness = 0;
+        child.children[1].material.color.set(0x999999);
+        child.children[1].material.ior = 3;
+        child.children[1].material.transmission = 1;
+        child.children[1].material.opacity = 0.8;
+        child.children[1].material.depthWrite = false;
+        child.children[1].material.depthTest = false;
+      }
+
+      if (child.name === 'Book') {
+        bookCover = child.children[0];
+
+        // adding texture to book
+        const bookTexture = new THREE.TextureLoader().load(
+          'textures/book-inner.jpg'
+        );
+        bookTexture.flipY = false;
+        child.material = new THREE.MeshStandardMaterial({
+          color: 0xffffff,
+          map: bookTexture,
+        });
+      }
+
+      if (child.name === 'SwitchBoard') {
+        lightSwitch = child.children[0];
+      }
+    });
+
+    scene.add(room.scene);
+    animate();
+
+    // add animation
+    mixer = new THREE.AnimationMixer(room.scene);
+    const clips = room.animations;
+    clipNames.forEach((clipName) => {
+      const clip = THREE.AnimationClip.findByName(clips, clipName);
+      if (clip) {
+        const action = mixer.clipAction(clip);
+        action.play();
+      }
+    });
+
+    loadIntroText();
+
+    // add event listeners
+    logoListener();
+    aboutMenuListener();
+    projectsMenuListener();
+    init3DWorldClickListeners();
+    initResponsive(room.scene);
+  },
+  function (error) {
+    console.error(error);
+  }
+);
+
+// ADD LIGHT
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+scene.add(ambientLight);
+const roomLight = new THREE.PointLight(0xffffff, 2.5, 10);
+roomLight.position.set(0.3, 2, 0.5);
+roomLight.castShadow = true;
+roomLight.shadow.radius = 5;
+roomLight.shadow.mapSize.width = 2048;
+roomLight.shadow.mapSize.height = 2048;
+roomLight.shadow.camera.far = 2.5;
+// roomLight.shadow.camera.fov = 100;
+roomLight.shadow.bias = -0.002;
+scene.add(roomLight);
+// add light for pc fans
+const fanLight1 = new THREE.PointLight(0xff0000, 30, 0.2);
+const fanLight2 = new THREE.PointLight(0x00ff00, 30, 0.12);
+const fanLight3 = new THREE.PointLight(0x00ff00, 30, 0.2);
+const fanLight4 = new THREE.PointLight(0x00ff00, 30, 0.2);
+const fanLight5 = new THREE.PointLight(0x00ff00, 30, 0.05);
+fanLight1.position.set(0, 0.29, -0.29);
+fanLight2.position.set(-0.15, 0.29, -0.29);
+fanLight3.position.set(0.21, 0.29, -0.29);
+fanLight4.position.set(0.21, 0.19, -0.29);
+fanLight5.position.set(0.21, 0.08, -0.29);
+scene.add(fanLight1);
+scene.add(fanLight2);
+scene.add(fanLight3);
+scene.add(fanLight4);
+scene.add(fanLight5);
+// add point light for text on wall
+const pointLight1 = new THREE.PointLight(0xff0000, 0, 1.1);
+const pointLight2 = new THREE.PointLight(0xff0000, 0, 1.1);
+const pointLight3 = new THREE.PointLight(0xff0000, 0, 1.1);
+const pointLight4 = new THREE.PointLight(0xff0000, 0, 1.1);
+pointLight1.position.set(-0.2, 0.6, 0.24);
+pointLight2.position.set(-0.2, 0.6, 0.42);
+pointLight3.position.set(-0.2, 0.6, 0.01);
+pointLight4.position.set(-0.2, 0.6, -0.14);
+scene.add(pointLight1);
+scene.add(pointLight2);
+scene.add(pointLight3);
+scene.add(pointLight4);
+
+const clock = new THREE.Clock();
+function animate() {
+  requestAnimationFrame(animate);
+  // controls.update();
+  if (mixer) {
+    mixer.update(clock.getDelta());
+  }
+  renderer.render(scene, camera);
+  // stats.update();
+}
+
+function loadIntroText() {
+  const loader = new FontLoader();
+  loader.load('fonts/unione.json', function (font) {
+    const textMaterials = [
+      new THREE.MeshPhongMaterial({ color: 0x171f27, flatShading: true }),
+      new THREE.MeshPhongMaterial({ color: 0xffffff }),
+    ];
+    const titleGeo = new TextGeometry('SCOOTER YU', {
+      font: font,
+      size: 0.08,
+      height: 0.01,
+    });
+    titleText = new THREE.Mesh(titleGeo, textMaterials);
+    titleText.rotation.y = Math.PI * 0.5;
+    titleText.position.set(-0.27, 0.55, 0.5);
+    scene.add(titleText);
+  });
+
+  loader.load('fonts/helvatica.json', function (font) {
+    const textMaterials = [
+      new THREE.MeshPhongMaterial({ color: 0x171f27, flatShading: true }),
+      new THREE.MeshPhongMaterial({ color: 0xffffff }),
+    ];
+    const subTitleGeo = new TextGeometry(
+      'Full-Stack Devloper / 3D Modeller / Animater',
+      {
+        font: font,
+        size: 0.018,
+        height: 0,
+      }
+    );
+    subtitleText = new THREE.Mesh(subTitleGeo, textMaterials);
+    subtitleText.rotation.y = Math.PI * 0.5;
+    subtitleText.position.set(-0.255, 0.5, 0.5);
+    scene.add(subtitleText);
+  });
+}
+
+function switchTheme(themeType) {
+  if (themeType === 'dark') {
+    lightSwitch.rotation.z = Math.PI / 7;
+    document.body.classList.remove('light-theme');
+    document.body.classList.add('dark-theme');
+
+    // main lights
+    gsap.to(roomLight.color, {
+      r: 0.27254901960784313,
+      g: 0.23137254901960785,
+      b: 0.6862745098039216,
+    });
+    gsap.to(ambientLight.color, {
+      r: 0.17254901960784313,
+      g: 0.23137254901960785,
+      b: 0.6862745098039216,
+    });
+    gsap.to(roomLight, {
+      intensity: 1.5,
+    });
+    gsap.to(ambientLight, {
+      intensity: 0.3,
+    });
+
+    // fan lights
+    gsap.to(fanLight5, {
+      distance: 0.07,
+    });
+
+    // text color
+    gsap.to(titleText.material[0].color, {
+      r: 8,
+      g: 8,
+      b: 8,
+      duration: 0,
+    });
+    gsap.to(titleText.material[1].color, {
+      r: 5,
+      g: 5,
+      b: 5,
+      duration: 0,
+    });
+    gsap.to(subtitleText.material[0].color, {
+      r: 8,
+      g: 8,
+      b: 8,
+      duration: 0,
+    });
+    gsap.to(subtitleText.material[1].color, {
+      r: 5,
+      g: 5,
+      b: 5,
+      duration: 0,
+    });
+
+    // text light
+    gsap.to(pointLight1, {
+      intensity: 0.6,
+    });
+    gsap.to(pointLight2, {
+      intensity: 0.6,
+    });
+    gsap.to(pointLight3, {
+      intensity: 0.6,
+    });
+    gsap.to(pointLight4, {
+      intensity: 0.6,
+    });
+  } else {
+    lightSwitch.rotation.z = 0;
+    document.body.classList.remove('dark-theme');
+    document.body.classList.add('light-theme');
+
+    // main light
+    gsap.to(roomLight.color, {
+      r: 1,
+      g: 1,
+      b: 1,
+    });
+    gsap.to(ambientLight.color, {
+      r: 1,
+      g: 1,
+      b: 1,
+    });
+    gsap.to(roomLight, {
+      intensity: 2.5,
+    });
+    gsap.to(ambientLight, {
+      intensity: 0.6,
+    });
+
+    // fan light
+    gsap.to(fanLight5, {
+      distance: 0.05,
+    });
+
+    // text color
+    gsap.to(titleText.material[0].color, {
+      r: 0.09019607843137255,
+      g: 0.12156862745098039,
+      b: 0.15294117647058825,
+      duration: 0,
+    });
+    gsap.to(titleText.material[1].color, {
+      r: 1,
+      g: 1,
+      b: 1,
+      duration: 0,
+    });
+    gsap.to(subtitleText.material[0].color, {
+      r: 0.09019607843137255,
+      g: 0.12156862745098039,
+      b: 0.15294117647058825,
+      duration: 0,
+    });
+    gsap.to(subtitleText.material[1].color, {
+      r: 1,
+      g: 1,
+      b: 1,
+      duration: 0,
+    });
+
+    // text light
+    gsap.to(pointLight1, {
+      intensity: 0,
+    });
+    gsap.to(pointLight2, {
+      intensity: 0,
+    });
+    gsap.to(pointLight3, {
+      intensity: 0,
+    });
+    gsap.to(pointLight4, {
+      intensity: 0,
+    });
+  }
+}
+
+function enableOrbitControls() {
+  controls.enabled = true;
+}
+
+function disableOrbitControls() {
+  controls.enabled = false;
+}
+
+function enableCloseBtn() {
+  document.getElementById('close-btn').style.display = 'block';
+}
+
+function disableCloseBtn() {
+  document.getElementById('close-btn').style.display = 'none';
+}
+
+function resetBookCover() {
+  if (!bookCover) return;
+
+  gsap.to(bookCover.rotation, {
+    x: 0,
+    duration: 1.5,
+  });
+}
+
+function resetProjects() {
+  if (projects.length === 0) return;
+
+  projects.forEach((project, i) => {
+    gsap.to(project.mesh.material, {
+      opacity: 0,
+      duration: 1,
+    });
+    gsap.to(project.mesh.position, {
+      y: project.y,
+      duration: 1,
+    });
+    gsap.to(project.mesh.scale, {
+      x: 0,
+      y: 0,
+      z: 0,
+      duration: 0,
+      delay: 1,
+    });
+  });
+}
+
+function resetCamera() {
+  resetBookCover();
+  resetProjects();
+  disableCloseBtn();
+  gsap.to(camera.position, {
+    ...defaultCameraPos,
+    duration: 1.5,
+  });
+  gsap.to(camera.rotation, {
+    ...defaultCamerRot,
+    duration: 1.5,
+  });
+  gsap.delayedCall(1.5, enableOrbitControls);
+
+  // reset dimmed light for about display
+  if (theme !== 'dark') {
+    gsap.to(roomLight, {
+      intensity: 2.5,
+      duration: 1.5,
+    });
+  }
+}
+
+function logoListener() {
+  document.getElementById('logo').addEventListener('click', function (e) {
+    e.preventDefault();
+    resetCamera();
+  });
+}
+
+function cameraToAbout() {
+  if (!bookCover) return;
+
+  gsap.to(camera.position, {
+    ...aboutCameraPos,
+    duration: 1.5,
+  });
+  gsap.to(camera.rotation, {
+    ...aboutCameraRot,
+    duration: 1.5,
+  });
+  gsap.to(bookCover.rotation, {
+    x: Math.PI,
+    duration: 1.5,
+    delay: 1.5,
+  });
+
+  // prevent about text clutter due to bright light
+  if (theme !== 'dark') {
+    gsap.to(roomLight, {
+      intensity: 1,
+      duration: 1.5,
+    });
+  }
+}
+
+function aboutMenuListener() {
+  document.getElementById('about-menu').addEventListener('click', function (e) {
+    e.preventDefault();
+    disableOrbitControls();
+    resetProjects();
+    cameraToAbout();
+    gsap.delayedCall(1.5, enableCloseBtn);
+  });
+}
+
